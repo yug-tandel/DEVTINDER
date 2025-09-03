f03f80dca3168177bc55fe295245203b7257e5b3
Yug Tandel <tandelyug0502@gmail.com>
2025-09-04 02:04:50 +0530

    next()

--- src/app.js
+++ src/app.js
@@ -2,47 +2,28 @@ const express = require("express");  //This imports the Express.js library into 
 
 const app = express(); // This creates a single instance of an Express application that defines your server and its behavior.
 
-// query parameter
-app.get("/user", (req, res) =>{
-    const query = req.query;
-    res.send((query));
-})
+app.get("/user", 
+    (req, res, next) =>{
+        console.log("user 1");
+        next()
 
-
-// Dynamic Route
-app.get("/product/:id/:name/:price", (req, res) =>{
-    const productObj = req.params;
-    res.send(productObj)
-})
-
-
-// Rejex
-// Any route that has 'a' inside it will work, eg-> /carbon
-app.get(/a/, (req, res) =>{
-    res.send("Rejex");
-})
-
-
-// Rejex 2
-// Anything in the start and ends with specified word will work eg-> butterfly
-app.get(/.*fly$/, (req, res) =>{
-    res.send("Rejex 2 get Request");
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    },
+    (req, res, next) =>{
+        console.log("user 2");
+        next()
+        console.log("2")
+        res.send("Request Handler Route 2")
+    },
+    (req, res, next) =>{
+        console.log("user 3");
+        res.send("Request Handler Route 3")
+        next()
+    },
+    (req, res,) =>{
+        console.log("user 4");
+        // res.send("Request handle Route 4")
+    }
+);
 
 //  //This starts the server, telling it to listen for incoming connections on port 3000
 

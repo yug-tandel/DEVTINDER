6142b61f32760d2326922b7931730b9efbe3b679
Yug Tandel <tandelyug0502@gmail.com>
2025-09-07 00:20:57 +0530

    MongoDB connection with express

--- note.md
+++ note.md
@@ -1,3 +1,15 @@
+const express = require("express"); //This imports the Express.js library into your file so you can use it.
+
+const app = express(); // This creates a single instance of an Express application that defines your server and its behavior.
+
+//This starts the server, telling it to listen for incoming connections on port 3000
+app.listen(3000, () => {
+    console.log("server is successfully listening on port 3000");
+});
+
+
+
+
 // Difference between Router Handler and Middleware
 
 //#Route Handler - In Node.js, particularly within frameworks like Express.js, a route handler is a function or a series of functions that are executed when a server receives a request for a specific URL path and HTTP method (e.g., GET, POST, PUT, DELETE). 
--- src/app.js
+++ src/app.js
@@ -1,35 +1,20 @@
-const express = require("express");  //This imports the Express.js library into your file so you can use it.
+const express = require("express");  
+const connectDB = require("./config/database")
+const app = express(); 
 
-const app = express(); // This creates a single instance of an Express application that defines your server and its behavior.
-const { adminAuth } = require("./middlewares/auth")
-
-app.use("/admin", adminAuth ); 
-
-app.get("/user", (req, res, next) =>{
-    res.send("User data Sent")
-})
-
-app.get("/admin/getAllData", (req, res, next) =>{
-     res.send("All Data Sent");
-})
-
-app.get("/admin/deleteUser", (req, res, next) =>{
-    res.send("Deleted a user");
-})
-
-
-
-
-
-
-
-
-
-//  //This starts the server, telling it to listen for incoming connections on port 3000
-
-app.listen(3000, () => {
+connectDB().then(() =>{
+    console.log("Database connection established...");
+    app.listen(3000, () => {
     console.log("server is successfully listening on port 3000");
 });
+}).catch((err)=>{
+    console.log("Database cannot be connected!!")
+})
+
+
+
+
+
 
 
 

4fce5f5cea81a6944e898257e8fe9eee18ba5ce8
Yug Tandel <tandelyug0502@gmail.com>
2025-09-07 16:50:42 +0530

    data added to database successfully

--- src/app.js
+++ src/app.js
@@ -1,6 +1,24 @@
 const express = require("express");  
 const connectDB = require("./config/database")
 const app = express(); 
+const User = require('./models/user');
+
+app.post("/signup", async (req, res) =>{
+    //Creating a new instance of the User model
+    const user = new User({
+        firstName: "Yug",
+        lastName: "Tandel",
+        emailId: "y@gmail.com",
+        password: 'y1'
+    })
+
+    await user.save();
+    res.send("User Added successfully!");
+});
+
+
+
+
 
 connectDB().then(() =>{
     console.log("Database connection established...");

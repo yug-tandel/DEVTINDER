6ce4bf4fba5b20fbee9bd5f1d7f4ddff09ca1080
Yug Tandel <tandelyug0502@gmail.com>
2025-09-07 23:45:00 +0530

    GET

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 const express = require("express");  
-const connectDB = require("./config/database")
 const app = express(); 
+const connectDB = require("./config/database")
 const User = require('./models/user');
 
 app.use(express.json())
 
+// POSTING DATA
 app.post("/signup", async (req, res) =>{
     //Creating a new instance of the User model
     const user = new User(req.body)
@@ -17,6 +18,36 @@ app.post("/signup", async (req, res) =>{
         res.status(400).send("error saving the user", err.message);
     }
 });
+
+// Get user by email
+app.get("/user", async (req, res) => {
+    const userEmail = req.body.emailId;
+
+    try {
+
+        const users = await User.find({emailId: userEmail})
+        if(users.length === 0){
+            res.send("NO Data found")
+        } else {
+            res.send(users);
+        }
+        
+    } catch(err) {
+        res.status(400).send("Something went wrong");
+    }
+})
+
+//Feed API - GET /feed - get all the users from the database
+app.get("/feed", async (req, res) =>{
+
+    try{
+        const allUsers = await User.find({})
+        res.send(allUsers);
+    } catch (err){
+        res.status(400).send("Something went wrong", err.message);
+    }
+
+})
 
 
 
